12ed13442d7e0840b6d273089d69af6eaef3eeeb
DESKTOP-DELL\apara <mykytazakh@gmail.com>
2022-02-10 09:35:10 +0200

    New branch for homework4-5

---
+++ homework(condition-methods)/Homework4-5/HW4-conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW4_conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
